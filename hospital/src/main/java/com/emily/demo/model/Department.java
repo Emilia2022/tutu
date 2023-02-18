c91098e11302f95610ec4b3de42397b940e7573b
Emilia2022 <106738248+Emilia2022@users.noreply.github.com>
2023-02-18 22:05:49 +0300

    1 temp

---
+++ hospital/src/main/java/com/emily/demo/model/Department.java
@@ -0,0 +1,16 @@
+package com.emily.demo.model;
+
+import lombok.*;
+
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Department {
+    private Long id;
+    private String name;
+
+}
